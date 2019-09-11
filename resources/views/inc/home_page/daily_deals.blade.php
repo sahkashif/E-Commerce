1497c037972103b939d0e580cbbd8d448bc85801
sahkashif <sahkashif@gmail.com>
2019-09-12 02:14:18 +0600

    daily sale on index is completed

--- resources/views/inc/home_page/daily_deals.blade.php
+++ resources/views/inc/home_page/daily_deals.blade.php
@@ -1,31 +1,33 @@
 
 <div class="product_area deals_product mb-50">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="section_title">
-                       <h2>Deal Of The Days</h2>
-                    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="section_title">
+                    <h2>Deal Of The Days</h2>
                 </div>
-            </div> 
-            <div class="row">
-                <div class="product_carousel product_column4 owl-carousel">
-                   <div class="col-lg-3">
+            </div>
+        </div> 
+        <div class="row">
+            <div class="product_carousel product_column4 owl-carousel">
+                @foreach ($onDealProducts as $product)
+                <div class="col-lg-3">
                         <article class="single_product">
                             <figure>
-                               <div class="product_name">
-                                   <h4><a href="product-countdown.html">Nostrum exercitationem itae neque nulla nec posuere sem</a></h4>
-                               </div>
-                               <div class="product_rating">
-                                   <ul>
-                                       <li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
-                                       <li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
-                                       <li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
-                                       <li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
-                                       <li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
-                                   </ul>
-                               </div>
-
+                                <div class="product_name">
+                                    <h4><a href="product-countdown.html">{{ $product->name }}</a></h4>
+                                </div>
+                                <div class="product_rating">
+                                    <div class="star-ratings">
+                                        <div class="fill-ratings" style="width: 50%;">
+                                            <span>★★★★★</span>
+                                        </div>
+                                        <div class="empty-ratings">
+                                            <span>★★★★★</span>
+                                        </div>
+                                    </div>
+                                </div>
+    
                                 <div class="product_thumb">
                                     <a class="primary_img" href="product-countdown.html"><img src="assets/img/product/product1.jpg" alt=""></a>
                                     <div class="label_product">
@@ -36,156 +38,17 @@
                                     </div>
                                 </div>
                                 <div class="price_box"> 
-                                    <span class="old_price">$86.00</span> 
-                                    <span class="current_price">$79.00</span>
+                                    <span class="old_price">${{ $product->price }}</span> 
+                                    <span class="current_price">${{ $product->present_price }}</span>
                                 </div>
                                 <div class="product_timing">
-                                    <div data-countdown="2022/12/15"></div>
+                                    <div data-countdown="{{ $product->sale_ended_at }}"></div>
                                 </div>
                             </figure>
                         </article>
                     </div>
-                    <div class="col-lg-3">
-                        <article class="single_product">
-                            <figure>
-                               <div class="product_name">
-                                   <h4><a href="product-countdown.html">Cillum dolore tortor nisl fermentum quam</a></h4>
-                               </div>
-                               <div class="product_rating">
-                                   <ul>
-                                       <li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
-                                       <li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
-                                       <li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
-                                       <li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
-                                       <li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
-                                   </ul>
-                               </div>
-
-                                <div class="product_thumb">
-                                    <a class="primary_img" href="product-countdown.html"><img src="assets/img/product/product2.jpg" alt=""></a>
-                                    <div class="label_product">
-                                        <span class="label_sale">Sale!</span>
-                                    </div>
-                                    <div class="quick_button">
-                                        <a href="#" data-toggle="modal" data-target="#modal_box"  title="quick view"> Quick View</a>
-                                    </div>
-                                </div>
-                                <div class="price_box"> 
-                                    <span class="old_price">$80.00</span> 
-                                    <span class="current_price">$70.00</span>
-                                </div>
-                                <div class="product_timing">
-                                    <div data-countdown="2022/12/15"></div>
-                                </div>
-                            </figure>
-                        </article>
-                    </div>
-                    <div class="col-lg-3">
-                        <article class="single_product">
-                            <figure>
-                               <div class="product_name">
-                                   <h4><a href="product-countdown.html">Laudantium enim fringilla dignissim ipsum primis</a></h4>
-                               </div>
-                               <div class="product_rating">
-                                   <ul>
-                                       <li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
-                                       <li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
-                                       <li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
-                                       <li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
-                                       <li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
-                                   </ul>
-                               </div>
-
-                                <div class="product_thumb">
-                                    <a class="primary_img" href="product-countdown.html"><img src="assets/img/product/product3.jpg" alt=""></a>
-                                    <div class="label_product">
-                                        <span class="label_sale">Sale!</span>
-                                    </div>
-                                    <div class="quick_button">
-                                        <a href="#" data-toggle="modal" data-target="#modal_box"  title="quick view"> Quick View</a>
-                                    </div>
-                                </div>
-                                <div class="price_box"> 
-                                    <span class="old_price">$65.00</span> 
-                                    <span class="current_price">$60.00</span>
-                                </div>
-                                <div class="product_timing">
-                                    <div data-countdown="2022/12/15"></div>
-                                </div>
-                            </figure>
-                        </article>
-                    </div>
-                    <div class="col-lg-3">
-                        <article class="single_product">
-                            <figure>
-                               <div class="product_name">
-                                   <h4><a href="product-countdown.html">Mirum est notare tellus eu nibh iaculis pretium</a></h4>
-                               </div>
-                               <div class="product_rating">
-                                   <ul>
-                                       <li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
-                                       <li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
-                                       <li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
-                                       <li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
-                                       <li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
-                                   </ul>
-                               </div>
-
-                                <div class="product_thumb">
-                                    <a class="primary_img" href="product-countdown.html"><img src="assets/img/product/product4.jpg" alt=""></a>
-                                    <div class="label_product">
-                                        <span class="label_sale">Sale!</span>
-                                    </div>
-                                    <div class="quick_button">
-                                        <a href="#" data-toggle="modal" data-target="#modal_box"  title="quick view"> Quick View</a>
-                                    </div>
-                                </div>
-                                <div class="price_box"> 
-                                    <span class="old_price">$62.00</span> 
-                                    <span class="current_price">$56.00</span>
-                                </div>
-                                <div class="product_timing">
-                                    <div data-countdown="2022/12/15"></div>
-                                </div>
-                            </figure>
-                        </article>
-                    </div>
-                    <div class="col-lg-3">
-                        <article class="single_product">
-                            <figure>
-                               <div class="product_name">
-                                   <h4><a href="product-countdown.html">Fusce ultricies  dolor vitae tristique suscipit</a></h4>
-                               </div>
-                               <div class="product_rating">
-                                   <ul>
-                                       <li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
-                                       <li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
-                                       <li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
-                                       <li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
-                                       <li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
-                                   </ul>
-                               </div>
-
-                                <div class="product_thumb">
-                                    <a class="primary_img" href="product-countdown.html"><img src="assets/img/product/product5.jpg" alt=""></a>
-                                    <div class="label_product">
-                                        <span class="label_sale">Sale!</span>
-                                    </div>
-                                    <div class="quick_button">
-                                        <a href="#" data-toggle="modal" data-target="#modal_box"  title="quick view"> Quick View</a>
-                                    </div>
-                                </div>
-                                <div class="price_box"> 
-                                    <span class="old_price">$87.00</span> 
-                                    <span class="current_price">$81.00</span>
-                                </div>
-                                <div class="product_timing">
-                                    <div data-countdown="2022/12/15"></div>
-                                </div>
-                            </figure>
-                        </article>
-                    </div>
-                </div>
+                @endforeach
             </div>
         </div>
     </div>
+</div>
